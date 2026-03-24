2156bc07e47b41184720d316f0a3e199f6cff690
eecs_havefun <jingxin2345@gmail.com>
2026-03-24 09:08:26 +0800

    9:08:main and utils

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 import numpy as np
 import prettytable as pt
 import torch
-import torch.autograd
 import torch.nn as nn
 import transformers
 from sklearn.metrics import precision_recall_fscore_support, f1_score
@@ -24,26 +23,40 @@ class Trainer(object):
         other_params = list(set(self.model.parameters()) - bert_params)
         no_decay = ['bias', 'LayerNorm.weight']
         params = [
-            {'params': [p for n, p in model.bert.named_parameters() if not any(nd in n for nd in no_decay)],
-             'lr': config.bert_learning_rate,
-             'weight_decay': config.weight_decay},
-            {'params': [p for n, p in model.bert.named_parameters() if any(nd in n for nd in no_decay)],
-             'lr': config.bert_learning_rate,
-             'weight_decay': 0.0},
-            {'params': other_params,
-             'lr': config.learning_rate,
-             'weight_decay': config.weight_decay},
+            {
+                'params': [
+                    p for n, p in model.bert.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                'lr': config.bert_learning_rate,
+                'weight_decay': config.weight_decay
+            },
+            {
+                'params': [
+                    p for n, p in model.bert.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                'lr': config.bert_learning_rate,
+                'weight_decay': 0.0
+            },
+            {
+                'params': other_params,
+                'lr': config.learning_rate,
+                'weight_decay': config.weight_decay
+            },
         ]
 
         self.optimizer = transformers.AdamW(
-    params,
-    lr=config.learning_rate,
-    weight_decay=config.weight_decay
-)
+            params,
+            lr=config.learning_rate,
+            weight_decay=config.weight_decay
+        )
 
-        self.scheduler = transformers.get_linear_schedule_with_warmup(self.optimizer,
-                                                                      num_warmup_steps=config.warm_factor * updates_total,
-                                                                      num_training_steps=updates_total)
+        self.scheduler = transformers.get_linear_schedule_with_warmup(
+            self.optimizer,
+            num_warmup_steps=config.warm_factor * updates_total,
+            num_training_steps=updates_total
+        )
 
     def train(self, epoch, data_loader):
         self.model.train()
@@ -51,16 +64,14 @@ class Trainer(object):
         pred_result = []
         label_result = []
 
-        for i, data_batch in enumerate(data_loader):
+        for _, data_batch in enumerate(data_loader):
             data_batch = [data.cuda() for data in data_batch[:-1]]
-
             bert_inputs, grid_labels, grid_mask2d, pieces2word, dist_inputs, sent_length = data_batch
 
             outputs = model(bert_inputs, grid_mask2d, dist_inputs, pieces2word, sent_length)
 
             grid_mask2d = grid_mask2d.clone()
             loss = self.criterion(outputs[grid_mask2d], grid_labels[grid_mask2d])
-
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), config.clip_grad_norm)
             self.optimizer.step()
@@ -80,38 +91,43 @@ class Trainer(object):
         label_result = torch.cat(label_result)
         pred_result = torch.cat(pred_result)
 
-        p, r, f1, _ = precision_recall_fscore_support(label_result.numpy(),
-                                                      pred_result.numpy(),
-                                                      average="macro")
+        p, r, f1, _ = precision_recall_fscore_support(
+            label_result.numpy(),
+            pred_result.numpy(),
+            average="macro"
+        )
 
         table = pt.PrettyTable(["Train {}".format(epoch), "Loss", "F1", "Precision", "Recall"])
-        table.add_row(["Label", "{:.4f}".format(np.mean(loss_list))] +
-                      ["{:3.4f}".format(x) for x in [f1, p, r]])
+        table.add_row(["Label", "{:.4f}".format(np.mean(loss_list))] + ["{:3.4f}".format(x) for x in [f1, p, r]])
         logger.info("\n{}".format(table))
         return f1
 
     def eval(self, epoch, data_loader, is_test=False):
         self.model.eval()
-
         pred_result = []
         label_result = []
 
         total_ent_r = 0
         total_ent_p = 0
         total_ent_c = 0
+
         with torch.no_grad():
-            for i, data_batch in enumerate(data_loader):
+            for _, data_batch in enumerate(data_loader):
                 entity_text = data_batch[-1]
                 data_batch = [data.cuda() for data in data_batch[:-1]]
+
                 bert_inputs, grid_labels, grid_mask2d, pieces2word, dist_inputs, sent_length = data_batch
-
                 outputs = model(bert_inputs, grid_mask2d, dist_inputs, pieces2word, sent_length)
+
                 length = sent_length
-
                 grid_mask2d = grid_mask2d.clone()
-
                 outputs = torch.argmax(outputs, -1)
-                ent_c, ent_p, ent_r, _ = utils.decode(outputs.cpu().numpy(), entity_text, length.cpu().numpy())
+
+                ent_c, ent_p, ent_r, _ = utils.decode(
+                    outputs.cpu().numpy(),
+                    entity_text,
+                    length.cpu().numpy()
+                )
 
                 total_ent_r += ent_r
                 total_ent_p += ent_p
@@ -126,57 +142,79 @@ class Trainer(object):
         label_result = torch.cat(label_result)
         pred_result = torch.cat(pred_result)
 
-        p, r, f1, _ = precision_recall_fscore_support(label_result.numpy(),
-                                                      pred_result.numpy(),
-                                                      average="macro")
+        p, r, f1, _ = precision_recall_fscore_support(
+            label_result.numpy(),
+            pred_result.numpy(),
+            average="macro"
+        )
         e_f1, e_p, e_r = utils.cal_f1(total_ent_c, total_ent_p, total_ent_r)
 
         title = "EVAL" if not is_test else "TEST"
-        logger.info('{} Label F1 {}'.format(title, f1_score(label_result.numpy(),
-                                                            pred_result.numpy(),
-                                                            average=None)))
+        logger.info('{} Label F1 {}'.format(title, f1_score(label_result.numpy(), pred_result.numpy(), average=None)))
 
         table = pt.PrettyTable(["{} {}".format(title, epoch), 'F1', "Precision", "Recall"])
         table.add_row(["Label"] + ["{:3.4f}".format(x) for x in [f1, p, r]])
         table.add_row(["Entity"] + ["{:3.4f}".format(x) for x in [e_f1, e_p, e_r]])
-
         logger.info("\n{}".format(table))
         return e_f1
 
     def predict(self, epoch, data_loader, data):
-        self.model.eval()
+        """
+        兼容原版 predict() 的同时，额外导出 ProcNet 友好的 typed entity 结构。
 
+        输出的每个样本同时包含：
+        - entity: 原版 [{text, type}, ...]
+        - procnet_entities: [{key, cluster_key, token_indices, b, e, type_id, score, head, ...}, ...]
+        """
+        self.model.eval()
         pred_result = []
         label_result = []
-
         result = []
 
         total_ent_r = 0
         total_ent_p = 0
         total_ent_c = 0
 
-        i = 0
+        batch_start = 0
         with torch.no_grad():
             for data_batch in data_loader:
-                sentence_batch = data[i:i+config.batch_size]
+                sentence_batch = data[batch_start: batch_start + config.batch_size]
                 entity_text = data_batch[-1]
                 data_batch = [data.cuda() for data in data_batch[:-1]]
+
                 bert_inputs, grid_labels, grid_mask2d, pieces2word, dist_inputs, sent_length = data_batch
 
-                outputs = model(bert_inputs, grid_mask2d, dist_inputs, pieces2word, sent_length)
-                length = sent_length
+                logits = model(bert_inputs, grid_mask2d, dist_inputs, pieces2word, sent_length)
+                probs = torch.softmax(logits, dim=-1)
+                outputs = torch.argmax(logits, -1)
 
+                length = sent_length
                 grid_mask2d = grid_mask2d.clone()
 
-                outputs = torch.argmax(outputs, -1)
-                ent_c, ent_p, ent_r, decode_entities = utils.decode(outputs.cpu().numpy(), entity_text, length.cpu().numpy())
+                ent_c, ent_p, ent_r, decode_entities = utils.decode(
+                    outputs.cpu().numpy(),
+                    entity_text,
+                    length.cpu().numpy()
+                )
 
-                for ent_list, sentence in zip(decode_entities, sentence_batch):
-                    sentence = sentence["sentence"]
-                    instance = {"sentence": sentence, "entity": []}
-                    for ent in ent_list:
-                        instance["entity"].append({"text": [sentence[x] for x in ent[0]],
-                                                   "type": config.vocab.id_to_label(ent[1])})
+                # 额外拿到 score / head / boundary 友好的导出结构
+                procnet_decoded = utils.decode_for_procnet(
+                    outputs.cpu().numpy(),
+                    probs.cpu().numpy(),
+                    length.cpu().numpy()
+                )
+
+                for local_idx, (ent_list, procnet_ent_list, sentence_record) in enumerate(
+                    zip(decode_entities, procnet_decoded, sentence_batch)
+                ):
+                    instance = utils.build_prediction_record(
+                        sentence_record=sentence_record,
+                        decoded_entities=ent_list,
+                        procnet_decoded_entities=procnet_ent_list,
+                        vocab=config.vocab,
+                        sample_idx=batch_start + local_idx,
+                        continuous_only=bool(getattr(config, "continuous_only", 1))
+                    )
                     result.append(instance)
 
                 total_ent_r += ent_r
@@ -188,29 +226,29 @@ class Trainer(object):
 
                 label_result.append(grid_labels.cpu())
                 pred_result.append(outputs.cpu())
-                i += config.batch_size
+
+                batch_start += config.batch_size
 
         label_result = torch.cat(label_result)
         pred_result = torch.cat(pred_result)
 
-        p, r, f1, _ = precision_recall_fscore_support(label_result.numpy(),
-                                                      pred_result.numpy(),
-                                                      average="macro")
+        p, r, f1, _ = precision_recall_fscore_support(
+            label_result.numpy(),
+            pred_result.numpy(),
+            average="macro"
+        )
         e_f1, e_p, e_r = utils.cal_f1(total_ent_c, total_ent_p, total_ent_r)
 
         title = "TEST"
-        logger.info('{} Label F1 {}'.format("TEST", f1_score(label_result.numpy(),
-                                                            pred_result.numpy(),
-                                                            average=None)))
+        logger.info('{} Label F1 {}'.format("TEST", f1_score(label_result.numpy(), pred_result.numpy(), average=None)))
 
         table = pt.PrettyTable(["{} {}".format(title, epoch), 'F1', "Precision", "Recall"])
         table.add_row(["Label"] + ["{:3.4f}".format(x) for x in [f1, p, r]])
         table.add_row(["Entity"] + ["{:3.4f}".format(x) for x in [e_f1, e_p, e_r]])
-
         logger.info("\n{}".format(table))
 
         with open(config.predict_path, "w", encoding="utf-8") as f:
-            json.dump(result, f, ensure_ascii=False)
+            json.dump(result, f, ensure_ascii=False, indent=2)
 
         return e_f1
 
@@ -237,14 +275,12 @@ if __name__ == '__main__':
     parser.add_argument('--biaffine_size', type=int)
 
     parser.add_argument('--dilation', type=str, help="e.g. 1,2,3")
-
     parser.add_argument('--emb_dropout', type=float)
     parser.add_argument('--conv_dropout', type=float)
     parser.add_argument('--out_dropout', type=float)
 
     parser.add_argument('--epochs', type=int)
     parser.add_argument('--batch_size', type=int)
-
     parser.add_argument('--clip_grad_norm', type=float)
     parser.add_argument('--learning_rate', type=float)
     parser.add_argument('--weight_decay', type=float)
@@ -252,15 +288,20 @@ if __name__ == '__main__':
     parser.add_argument('--bert_name', type=str)
     parser.add_argument('--bert_learning_rate', type=float)
     parser.add_argument('--warm_factor', type=float)
-
     parser.add_argument('--use_bert_last_4_layers', type=int, help="1: true, 0: false")
-
     parser.add_argument('--seed', type=int)
+
+    # ProcNet 对接相关的最小开关
+    parser.add_argument(
+        '--continuous_only',
+        type=int,
+        default=1,
+        help="1 表示仅导出连续实体；0 表示保留不连续实体的 token_indices。"
+    )
 
     args = parser.parse_args()
 
     config = config.Config(args)
-
     logger = utils.get_logger(config.dataset)
     logger.info(config)
     config.logger = logger
@@ -277,14 +318,15 @@ if __name__ == '__main__':
 
     logger.info("Loading Data")
     datasets, ori_data = data_loader.load_data_bert(config)
-
     train_loader, dev_loader, test_loader = (
-        DataLoader(dataset=dataset,
-                   batch_size=config.batch_size,
-                   collate_fn=data_loader.collate_fn,
-                   shuffle=i == 0,
-                   num_workers=4,
-                   drop_last=i == 0)
+        DataLoader(
+            dataset=dataset,
+            batch_size=config.batch_size,
+            collate_fn=data_loader.collate_fn,
+            shuffle=i == 0,
+            num_workers=4,
+            drop_last=i == 0
+        )
         for i, dataset in enumerate(datasets)
     )
 
@@ -292,23 +334,26 @@ if __name__ == '__main__':
 
     logger.info("Building Model")
     model = Model(config)
-
     model = model.cuda()
 
     trainer = Trainer(model)
 
     best_f1 = 0
     best_test_f1 = 0
+
     for i in range(config.epochs):
         logger.info("Epoch: {}".format(i))
         trainer.train(i, train_loader)
         f1 = trainer.eval(i, dev_loader)
         test_f1 = trainer.eval(i, test_loader, is_test=True)
+
         if f1 > best_f1:
             best_f1 = f1
             best_test_f1 = test_f1
             trainer.save(config.save_path)
+
     logger.info("Best DEV F1: {:3.4f}".format(best_f1))
     logger.info("Best TEST F1: {:3.4f}".format(best_test_f1))
+
     trainer.load(config.save_path)
     trainer.predict("Final", test_loader, ori_data[-1])
--- utils.py
+++ utils.py
@@ -8,8 +8,11 @@ def get_logger(dataset):
     pathname = "./log/{}_{}.txt".format(dataset, time.strftime("%m-%d_%H-%M-%S"))
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s",
-                                  datefmt='%Y-%m-%d %H:%M:%S')
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s: %(message)s",
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
     file_handler = logging.FileHandler(pathname)
     file_handler.setLevel(logging.DEBUG)
@@ -21,7 +24,6 @@ def get_logger(dataset):
 
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
-
     return logger
 
 
@@ -36,67 +38,267 @@ def load_file(path):
     return data
 
 
-def convert_index_to_text(index, type):
+def convert_index_to_text(index, type_id):
     text = "-".join([str(i) for i in index])
-    text = text + "-#-{}".format(type)
+    text = text + "-#-{}".format(type_id)
     return text
 
 
 def convert_text_to_index(text):
-    index, type = text.split("-#-")
+    index, type_id = text.split("-#-")
     index = [int(x) for x in index.split("-")]
-    return index, int(type)
+    return index, int(type_id)
 
 
 def decode(outputs, entities, length):
     class Node:
         def __init__(self):
-            self.THW = []                # [(tail, type)]
-            self.NNW = defaultdict(set)   # {(head,tail): {next_index}}
+            self.THW = []  # [(tail, type_id)]
+            self.NNW = defaultdict(set)  # {(head, tail): {next_index}}
 
     ent_r, ent_p, ent_c = 0, 0, 0
     decode_entities = []
     q = deque()
+
     for instance, ent_set, l in zip(outputs, entities, length):
         predicts = []
         nodes = [Node() for _ in range(l)]
+
         for cur in reversed(range(l)):
             heads = []
-            for pre in range(cur+1):
+            for pre in range(cur + 1):
                 # THW
-                if instance[cur, pre] > 1: 
+                if instance[cur, pre] > 1:
                     nodes[pre].THW.append((cur, instance[cur, pre]))
                     heads.append(pre)
+
                 # NNW
                 if pre < cur and instance[pre, cur] == 1:
                     # cur node
                     for head in heads:
-                        nodes[pre].NNW[(head,cur)].add(cur)
+                        nodes[pre].NNW[(head, cur)].add(cur)
                     # post nodes
-                    for head,tail in nodes[cur].NNW.keys():
+                    for head, tail in nodes[cur].NNW.keys():
                         if tail >= cur and head <= pre:
-                            nodes[pre].NNW[(head,tail)].add(cur)
+                            nodes[pre].NNW[(head, tail)].add(cur)
+
             # entity
-            for tail,type_id in nodes[cur].THW:
+            for tail, type_id in nodes[cur].THW:
                 if cur == tail:
                     predicts.append(([cur], type_id))
                     continue
+
                 q.clear()
                 q.append([cur])
                 while len(q) > 0:
                     chains = q.pop()
-                    for idx in nodes[chains[-1]].NNW[(cur,tail)]:
+                    for idx in nodes[chains[-1]].NNW[(cur, tail)]:
                         if idx == tail:
                             predicts.append((chains + [idx], type_id))
                         else:
                             q.append(chains + [idx])
-        
+
         predicts = set([convert_index_to_text(x[0], x[1]) for x in predicts])
         decode_entities.append([convert_text_to_index(x) for x in predicts])
+
         ent_r += len(ent_set)
         ent_p += len(predicts)
         ent_c += len(predicts.intersection(ent_set))
+
     return ent_c, ent_p, ent_r, decode_entities
+
+
+def _mean(scores):
+    if not scores:
+        return 0.0
+    return float(sum(scores) / len(scores))
+
+
+def is_contiguous(indices):
+    if not indices:
+        return False
+    return list(indices) == list(range(indices[0], indices[-1] + 1))
+
+
+def build_entity_text(sentence_tokens, token_indices):
+    pieces = [str(sentence_tokens[i]) for i in token_indices]
+    if not pieces:
+        return ""
+
+    # 中文字符级或字粒度输入通常直接拼接；英文/词粒度更适合空格连接
+    if all(len(piece) == 1 for piece in pieces):
+        return "".join(pieces)
+    return " ".join(pieces)
+
+
+def _safe_meta_get(record, keys, default=None):
+    if not isinstance(record, dict):
+        return default
+    for key in keys:
+        if key in record:
+            return record[key]
+    return default
+
+
+def decode_for_procnet(outputs, scores, length):
+    """
+    在原 decode 的基础上，额外导出 ProcNet 更需要的结构：
+    - token_indices
+    - type_id
+    - score: 基于 THW + NNW 边的平均置信度
+    - head: 默认取 token_indices[0]
+
+    注意：
+    1. 这里不依赖 gold entities，因此适合直接用于 test/predict 导出。
+    2. 对不连续实体也会保留 token_indices；Phase 1 可在上层用 continuous_only 过滤。
+    """
+    class Node:
+        def __init__(self):
+            self.THW = []  # [(tail, type_id, thw_score)]
+            self.NNW = defaultdict(dict)  # {(head, tail): {next_index: edge_score}}
+
+    decode_entities = []
+    q = deque()
+
+    for instance, prob_instance, l in zip(outputs, scores, length):
+        predicts = {}
+        nodes = [Node() for _ in range(l)]
+
+        for cur in reversed(range(l)):
+            heads = []
+            for pre in range(cur + 1):
+                # THW: [tail, head] -> type_id
+                if instance[cur, pre] > 1:
+                    type_id = int(instance[cur, pre])
+                    thw_score = float(prob_instance[cur, pre, type_id])
+                    nodes[pre].THW.append((cur, type_id, thw_score))
+                    heads.append(pre)
+
+                # NNW: [pre, cur] == 1
+                if pre < cur and instance[pre, cur] == 1:
+                    nnw_score = float(prob_instance[pre, cur, 1])
+
+                    # 当前 pre 直接连接到 cur
+                    for head in heads:
+                        old_score = nodes[pre].NNW[(head, cur)].get(cur, -1.0)
+                        if nnw_score > old_score:
+                            nodes[pre].NNW[(head, cur)][cur] = nnw_score
+
+                    # 从 cur 继承“以 cur 作为下一跳”的可达路径
+                    for head, tail in nodes[cur].NNW.keys():
+                        if tail >= cur and head <= pre:
+                            old_score = nodes[pre].NNW[(head, tail)].get(cur, -1.0)
+                            if nnw_score > old_score:
+                                nodes[pre].NNW[(head, tail)][cur] = nnw_score
+
+            for tail, type_id, thw_score in nodes[cur].THW:
+                if cur == tail:
+                    key = convert_index_to_text([cur], type_id)
+                    predicts[key] = {
+                        "token_indices": [cur],
+                        "type_id": int(type_id),
+                        "score": thw_score,
+                        "head": cur,
+                    }
+                    continue
+
+                q.clear()
+                q.append(([cur], [thw_score]))
+
+                while len(q) > 0:
+                    chains, score_trace = q.pop()
+                    next_nodes = nodes[chains[-1]].NNW[(cur, tail)]
+
+                    for idx, edge_score in next_nodes.items():
+                        new_chain = chains + [idx]
+                        new_scores = score_trace + [edge_score]
+
+                        if idx == tail:
+                            key = convert_index_to_text(new_chain, type_id)
+                            final_score = _mean(new_scores)
+                            old = predicts.get(key)
+                            if old is None or final_score > old["score"]:
+                                predicts[key] = {
+                                    "token_indices": list(new_chain),
+                                    "type_id": int(type_id),
+                                    "score": final_score,
+                                    "head": int(new_chain[0]),
+                                }
+                        else:
+                            q.append((new_chain, new_scores))
+
+        decode_entities.append(list(predicts.values()))
+
+    return decode_entities
+
+
+def build_prediction_record(
+    sentence_record,
+    decoded_entities,
+    procnet_decoded_entities,
+    vocab,
+    sample_idx=0,
+    continuous_only=True,
+):
+    """
+    将官方 W2NER 的 predict 输出扩展为 ProcNet 更易消费的结构。
+    不要求你现在就改 data_loader；只要原始 json 里有 doc_id / sent_id，就会被自动带出。
+    """
+    if isinstance(sentence_record, dict):
+        sentence_tokens = sentence_record.get("sentence", [])
+    else:
+        sentence_tokens = sentence_record
+
+    doc_id = _safe_meta_get(sentence_record, ["doc_id", "docid", "guid", "id", "doc_key"], None)
+    sent_id = _safe_meta_get(sentence_record, ["sent_id", "sentence_id", "sid"], sample_idx)
+
+    instance = {
+        "doc_id": doc_id,
+        "sent_id": sent_id,
+        "sentence": sentence_tokens,
+        "entity": [],
+        "procnet_entities": [],
+    }
+
+    for ent in decoded_entities:
+        token_indices, type_id = ent
+        instance["entity"].append({
+            "text": [sentence_tokens[x] for x in token_indices],
+            "type": vocab.id_to_label(type_id),
+        })
+
+    for ent in procnet_decoded_entities:
+        token_indices = list(ent["token_indices"])
+        type_id = int(ent["type_id"])
+
+        if continuous_only and (not is_contiguous(token_indices)):
+            continue
+
+        b = token_indices[0]
+        e = token_indices[-1] + 1
+
+        if continuous_only:
+            key = [b, e, type_id]
+            cluster_key = [b, e, type_id]
+        else:
+            key = [token_indices, type_id]
+            cluster_key = [token_indices, type_id]
+
+        instance["procnet_entities"].append({
+            "key": key,
+            "cluster_key": cluster_key,
+            "token_indices": token_indices,
+            "b": b,
+            "e": e,
+            "type_id": type_id,
+            "type": vocab.id_to_label(type_id),
+            "score": round(float(ent["score"]), 6),
+            "head": int(ent["head"]),
+            "text": build_entity_text(sentence_tokens, token_indices),
+        })
+
+    instance["procnet_entities"].sort(key=lambda x: (x["b"], x["e"], x["type_id"]))
+    return instance
 
 
 def cal_f1(c, p, r):
